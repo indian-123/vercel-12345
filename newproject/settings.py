0cc9f5ee44841b185316b3fb3df7fd253ca97eee
indian9361072610 <111270826+indian9361072610@users.noreply.github.com>
2022-12-24 03:33:14 -0800

    Update settings.py

--- newproject/settings.py
+++ newproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'i1QC2CErv0xbifYGWeL0',
+        'HOST':'containers-us-west-39.railway.app',
+        'PORT':'8078',
     }
 }
 
